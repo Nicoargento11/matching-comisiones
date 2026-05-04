834ed0881481e1da9c441547ae0a40df2897498d
Nicolas Valdes <valdes.nicoedu@gmail.com>
2026-05-03 23:44:19 -0300

    refactor: eliminar módulos stubs vacíos (académico, intercambios, notificaciones)
    Eran stubs sin lógica real. Sus imports ya fueron removidos de AppModule en el commit anterior.

--- apps/api/src/modules/intercambios/intercambios.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('intercambios')
-export class IntercambiosController {}
--- apps/api/src/modules/intercambios/intercambios.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IntercambiosController } from './intercambios.controller';
-import { IntercambiosService } from './intercambios.service';
-
-@Module({
-  controllers: [IntercambiosController],
-  providers: [IntercambiosService],
-})
-export class IntercambiosModule {}
